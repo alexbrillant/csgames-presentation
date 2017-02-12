3a30b0168108aa5e2d4a984e70c712b31e086f61
alexbrillant <abrillant23@gmail.com>
2017-02-11 22:20:36 -0500

    cleaned navigation router

--- App/Navigation/NavigationRouter.js
+++ App/Navigation/NavigationRouter.js
@@ -5,31 +5,15 @@ import { Scene, Router } from 'react-native-router-flux'
 import Styles from './Styles/NavigationContainerStyle'
 import NavigationDrawer from './NavigationDrawer'
 import NavItems from './NavItems'
-import CustomNavBar from '../Navigation/CustomNavBar'
 import QuotesNavBar from '../Navigation/QuotesNavBar'
 
 // screens identified by the router
 import PresentationScreen from '../Containers/PresentationScreen'
-import AllComponentsScreen from '../Containers/AllComponentsScreen'
-import UsageExamplesScreen from '../Containers/UsageExamplesScreen'
-import LoginScreen from '../Containers/LoginScreen'
-import ListviewExample from '../Containers/ListviewExample'
-import ListviewGridExample from '../Containers/ListviewGridExample'
-import ListviewSectionsExample from '../Containers/ListviewSectionsExample'
-import ListviewSearchingExample from '../Containers/ListviewSearchingExample'
-import MapviewExample from '../Containers/MapviewExample'
-import APITestingScreen from '../Containers/APITestingScreen'
-import ThemeScreen from '../Containers/ThemeScreen'
-import DeviceInfoScreen from '../Containers/DeviceInfoScreen'
 import LayoutAnimationScreen from '../Containers/LayoutAnimationScreen'
 import ReactComponentScreen from '../Containers/ReactComponentScreen'
 import ReduxSagaScreen from '../Containers/ReduxSagaScreen'
 import ListviewAuthors from '../Containers/ListViewAuthors'
 import QuoteScreen from '../Containers/QuoteScreen'
-
-/* **************************
-* Documentation: https://github.com/aksonov/react-native-router-flux
-***************************/
 
 class NavigationRouter extends Component {
   render () {
@@ -38,24 +22,11 @@ class NavigationRouter extends Component {
         <Scene key='drawer' component={NavigationDrawer} open={false}>
           <Scene key='drawerChildrenWrapper' navigationBarStyle={Styles.navBar} titleStyle={Styles.title} leftButtonIconStyle={Styles.leftButton} rightButtonTextStyle={Styles.rightButton}>
             <Scene initial key='presentationScreen' component={PresentationScreen} title='React and Redux' renderLeftButton={NavItems.hamburgerButton} />
-            <Scene key='componentExamples' component={AllComponentsScreen} title='Components' />
-            <Scene key='usageExamples' component={UsageExamplesScreen} title='Usage' rightTitle='Example' onRight={() => window.alert('Example Pressed')} />
-            <Scene key='login' component={LoginScreen} title='Login' hideNavBar />
-            <Scene key='listviewExample' component={ListviewExample} title='Listview Example' />
-            <Scene key='listviewGridExample' component={ListviewGridExample} title='Listview Grid' />
-            <Scene key='listviewSectionsExample' component={ListviewSectionsExample} title='Listview Sections' />
-            <Scene key='listviewSearchingExample' component={ListviewSearchingExample} title='Listview Searching' navBar={CustomNavBar} />
-            <Scene key='mapviewExample' component={MapviewExample} title='Mapview Example' />
-            <Scene key='apiTesting' component={APITestingScreen} title='API Testing' />
-            <Scene key='theme' component={ThemeScreen} title='Theme' />
             <Scene key='layoutAnimation' component={LayoutAnimationScreen} title='Layout Animation' />
             <Scene key='reduxSaga' component={ReduxSagaScreen} title='Redux Saga' />
             <Scene key='reduxReducer' component={ListviewAuthors} title='Redux reducer' navBar={QuotesNavBar} />
             <Scene key='reactComponent' component={ReactComponentScreen} title='React component' />
             <Scene key='quote' component={QuoteScreen} title='Quote Screen' />
-
-            {/* Custom navigation bar example */}
-            <Scene key='deviceInfo' component={DeviceInfoScreen} title='Device Info' />
           </Scene>
         </Scene>
       </Router>
